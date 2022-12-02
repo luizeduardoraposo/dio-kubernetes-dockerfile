df7527c354b18551cea5752b83fab2bba2ee812b
Luiz Eduardo Raposo <luizeduardoraposo@protonmail.com>
2022-12-02 08:16:01 -0300

    first commit

---
+++ script.bat
@@ -0,0 +1,17 @@
+echo "Creating images"
+
+docker build -t denilsonbonatti/projeto-backend:1.0 backend/.
+docker build -t denilsonbonatti/projeto-database:1.0 database/.
+
+echo "Pushing images"
+
+docker push denilsonbonatti/projeto-backend:1.0
+docker push denilsonbonatti/projeto-database:1.0
+
+echo "Creating cluster services"
+
+kubectl apply -f ./service.yml
+
+echo "Creating deployments"
+
+kubectl apply -f ./deployment.yml
